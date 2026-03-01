6a169b1c6890e6888f3a1c1ab16df1c29da01e26
Soumyajit-io <soumyajit010805@gmail.com>
2026-03-01 13:43:21 +0530

    added init and chat function

--- agent/main.py
+++ agent/main.py
@@ -1,28 +1,41 @@
 import typer
+from .index import get_vector
+from .aiagent import chatbot
+from langchain_core.messages import HumanMessage
+
 app = typer.Typer()
 
-
 @app.command()
-def init():
-   pass
-
-
-@app.command()
-def ask(Question:str) :
-   pass
+def init(file_path):
+   """
+   Convert your codebase into vectors
+   """
+   get_vector(file_path)
 
 
 @app.command()
-def chat(Question:str) :
-   pass
+def chat() :
+   """
+   chat with the agent 
+   """
+   config = {'configurable':{'thread_id':12}}
+   while True:
+    user_msg = input("Ask anything: ")
+    result = chatbot.invoke({"messages": [HumanMessage(content=(user_msg))]},config=config)
+    print('AI: ',result["messages"][-1].content)
 
 
-@app.command()
-def reindex(Question:str) :
-   pass
+# @app.command()
+# def ask(Question:str) :
+#    pass
 
+# @app.command()
+# def reindex(Question:str) :
+#    pass
 
-@app.command()
-def explain(Question:str) :
-   pass
+# @app.command()
+# def explain(Question:str) :
+#    pass
 
+if __name__ == "__main__":
+  app()
